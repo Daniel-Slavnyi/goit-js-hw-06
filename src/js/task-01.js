5f318b6fe43f335018679a98f22acb13c84b8d43
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-01 00:03:24 +0300

    add task 1, 2, 3

---
+++ src/js/task-01.js
@@ -0,0 +1,17 @@
+'Use strict';
+
+const categories = document.querySelectorAll('li.item'),
+      arrCategories = [...categories];
+
+console.log('Number of categories:', categories.length);
+
+function getTextOfTitle(arr) {
+    arr.map((el => {
+        const title = el.firstElementChild.textContent;
+        const num = el.querySelectorAll('li').length;
+
+        return console.log(`Category: ${title} / Elements: ${num}`);
+    }));
+}
+
+getTextOfTitle(arrCategories);
