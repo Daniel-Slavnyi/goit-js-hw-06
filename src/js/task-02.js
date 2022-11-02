8eea36fbe1a58de0f557074585f6b8f48667b9e0
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-03 01:47:08 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -10,9 +10,19 @@ const ingredients = [
 const listEl = document.querySelector('#ingredients');
 
 
-ingredients.forEach(el => {
-    const itemEl = document.createElement('li');
-    itemEl.classList.add('item');
-    itemEl.textContent = el;
-    listEl.append(itemEl);
+// ingredients.forEach(el => {
+//     const itemEl = document.createElement('li');
+//     itemEl.classList.add('item');
+//     itemEl.textContent = el;
+//     listEl.append(itemEl);
+// });
+
+const itemsEl = ingredients.map(el => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = el;
+
+  return itemEl;
 });
+
+listEl.append(...itemsEl);
