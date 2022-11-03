01bb4cb01b1d0f9aa0b997c3fecc5f0b5f1e390a
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-03 21:34:24 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+"Use strict";
+
+const formEl = document.querySelector('.login-form');
+
+const onFormElSubmit = (e) => {
+    e.preventDefault();
+    const objData = {}; 
+
+    const { elements: { email, password } } = e.target;
+    if (email.value === "" || password.value === '') {
+        alert('please, write down the ariaes');
+    }
+    
+    objData.email = email.value;
+    objData.password = password.value;
+
+    e.target.reset();
+    console.log(objData);
+};
+
+formEl.addEventListener('submit', onFormElSubmit);
 
