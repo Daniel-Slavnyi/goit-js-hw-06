2dabed7af69c665f903099de68e17f4dda6dc81f
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-03 21:43:40 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color'),
+      valueColor = document.querySelector('.color');
+
+const onBtnElClick = (e) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  
+  valueColor.innerHTML = getRandomHexColor();
+};
+
+btnEl.addEventListener('click', onBtnElClick);
+
