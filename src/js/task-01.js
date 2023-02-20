c4a8913e90478b98d638d45d7449b847e64cce3f
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-02-20 14:53:19 +0200

    add change

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,49 @@
-'use strict';
+"use strict";
 
-const categories = document.querySelectorAll('li.item');
+const categories = document.querySelectorAll("li.item");
 
-console.log('Number of categories:', categories.length);
+console.log("Number of categories:", categories.length);
 
-categories.forEach(i => {
-    const title = i.firstElementChild.textContent;
-    const num = i.querySelectorAll('li').length;
+categories.forEach((i) => {
+  const title = i.firstElementChild.textContent;
+  const num = i.querySelectorAll("li").length;
 
-    console.log(`Category: ${title} / Elements: ${num}`);
+  console.log(`Category: ${title} / Elements: ${num}`);
 });
+
+fetch("https://pokeapi.co/api/v2/ability/?limit=20&offset=20")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+// const horses = [
+//   "Secretariat",
+//   "Eclipse",
+//   "WestAustralian",
+//   "FlyingFox",
+//   "Sebiscuit",
+// ];
+
+// function getRandomTime(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// function run(horse) {
+//   return new Promise((resolve) => {
+//     const time = getRandomTime(2000, 3500);
+
+//     setTimeout(() => {
+//       resolve({ horse, time });
+//     }, time);
+//   });
+// }
+
+// const promises = horses.map((horse) => run(horse));
+
+// Promise.race(promises).then(({ horse, time }) =>
+//   console.log(
+//     `%c 🥇 Our winner is ${horse}, she done the race for ${time} sec`,
+//     "color: tomato; font-size: 14px"
+//   )
+// );
+
+// Promise.all(promises).then((all) => console.log(all));
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,23 +3,21 @@ function getRandomHexColor() {
 }
 
 const inputEl = document.querySelector('input[type="number"]'),
-  btnAdd = document.querySelector('[data-create]'),
-  btnRem = document.querySelector('[data-destroy]'),
-  boxEl = document.querySelector('#boxes');
-      
-
+  btnAdd = document.querySelector("[data-create]"),
+  btnRem = document.querySelector("[data-destroy]"),
+  boxEl = document.querySelector("#boxes");
 
 function createBoxes(amount) {
   let size = 30;
   let boxes = [];
-  
+
   for (let i = 0; i < amount; i += 1) {
     size += 10;
 
-    const boxEl = document.createElement('div');
-    boxEl.classList.add('box');
+    const boxEl = document.createElement("div");
+    boxEl.classList.add("box");
     boxEl.style.width = size + "px";
-    boxEl.style.height = size + 'px';
+    boxEl.style.height = size + "px";
     boxEl.style.backgroundColor = getRandomHexColor();
 
     boxes.push(boxEl);
@@ -30,12 +28,12 @@ function createBoxes(amount) {
 
 const onBtnAddClick = () => {
   createBoxes(Number(inputEl.value));
-  inputEl.value = '';
+  inputEl.value = "";
 };
 
 const onBtnRemClick = () => {
   boxEl.innerHTML = "";
 };
 
-btnAdd.addEventListener('click', onBtnAddClick);
-btnRem.addEventListener('click', onBtnRemClick);
+btnAdd.addEventListener("click", onBtnAddClick);
+btnRem.addEventListener("click", onBtnRemClick);
